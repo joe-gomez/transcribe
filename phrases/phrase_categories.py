e0da68c1ec35af18a04818e02dac62670bf1ce4b
joe-gomez <joe.gomez@lis.ac.uk>
2025-06-19 18:40:43 +0200

    Create phrase_categories.py

---
+++ phrases/phrase_categories.py
@@ -0,0 +1,31 @@
+phrase_categories = {
+    "individualising": {
+        "color": "#fffa65",
+        "phrases": [
+            "you", "you're", "your", "yours", "yourself", "yourselves",
+            "you should", "you must", "you have to", "you need to", "you can",
+            # ... rest of your phrases ...
+        ]
+    },
+    "collective_we": {
+        "color": "#8ad6ff",
+        "phrases": [
+            "we're messing up", "we're ruining", "we're destroying", "we're failing",
+            # ... rest of your phrases ...
+        ]
+    },
+    "greenwashing": {
+        "color": "#b0ffb0",
+        "phrases": [
+            "natural", "nature's", "healthy", "healthier planet", "sustainable", "eco-friendly",
+            # ... rest of your phrases ...
+        ]
+    },
+    "moral_metaphors": {
+        "color": "#ffb0b0",
+        "phrases": [
+            "no planet B", "mother nature", "mother earth", "earth is hurting",
+            # ... rest of your phrases ...
+        ]
+    }
+}
